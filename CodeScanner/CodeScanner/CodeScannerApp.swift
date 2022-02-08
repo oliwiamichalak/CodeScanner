b21bc20fb54bfc3f8d33238df93e80c9a343f27c
oliwiamich <omichalak@magicseaweed.com>
2022-02-08 16:51:21 +0000

    feat: project init

---
+++ CodeScanner/CodeScanner/CodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeScannerApp.swift
+//  CodeScanner
+//
+//  Created by Oliwia Michalak on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeScannerView()
+        }
+    }
+}
---
+++ CodeScanner/CodeScanner/CodeScannerView.swift
@@ -0,0 +1,20 @@
+//
+//  CodeScannerView.swift
+//  CodeScanner
+//
+//  Created by Oliwia Michalak on 08/02/2022.
+//
+
+import SwiftUI
+
+struct CodeScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CodeScannerView()
+    }
+}
